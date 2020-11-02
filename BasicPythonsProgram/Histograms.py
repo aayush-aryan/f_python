c01a91793d9db032a472d8156877376634c1a21e
aayush-aryan <log2aayusharyan@gmail.com>
2020-11-02 16:17:28 +0530

    added. createe a histogram from given link

---
+++ BasicPythonsProgram/Histograms.py
@@ -0,0 +1,22 @@
+"""
+author - ${Aayush Aryan}
+date - ${02-11-2020}
+time - ${2:30}
+package - ${Basicpython}
+
+Title -" Write a Python program to create a histogram from a given list of integers."
+
+"""
+try:
+ def histogram(listElements):
+    for number in listElements :
+        output = ''
+        temVariable = number
+        while(temVariable > 0):
+          output = output + '*'
+          temVariable = temVariable - 1
+        print(output)
+
+ histogram([2, 3, 6, 5])
+except:
+    print("InvalidEntryOfItems")
