68c2c36081dd39f1ab9e38575c26476719dd1197
aayush-aryan <log2aayusharyan@gmail.com>
2020-11-02 14:05:50 +0530

    [aayush]added. a program to display color from list and tuples

---
+++ BasicPythonsProgram/DisplayColorList.py
@@ -0,0 +1,15 @@
+"""
+author - ${Aayush Aryan}
+date - ${02-11-2020}
+time - ${11:45}
+package - ${Basicpython}
+
+Title -" Write a Python program to display the first and last colors from the following list.
+color_list = ["Red","Green","White" ,"Black"]"
+
+"""
+try:
+ color_list = ["Red","Green","White" ,"Black"]
+ print("required color list are  : " "%s %s"%(color_list[0],color_list[-1]))
+except:
+    print("checkedListTupleIndexingValue")
