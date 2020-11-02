c639be933b6db1835d075c8c2cc5aae2dadf0f89
aayush-aryan <log2aayusharyan@gmail.com>
2020-11-02 14:26:32 +0530

    added. print the document of python built in function and return absolute value of argument

---
+++ BasicPythonsProgram/Document.py
@@ -0,0 +1,18 @@
+"""
+author - ${Aayush Aryan}
+date - ${02-11-2020}
+time - ${11:45}
+package - ${Basicpython}
+
+Title -" Write a Python program to print the documents (syntax, description etc.) of Python built-in
+function(s).
+Sample function : abs()
+Expected Result :
+abs(number) -> number
+Return the absolute value of the argument."
+
+"""
+try:
+ print(abs.__doc__)
+except :
+    print("enterValidDocumentSyntax")
