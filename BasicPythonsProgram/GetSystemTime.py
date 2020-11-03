3598fe61a2ffc1dbe465a215febb2021630fad45
aayush-aryan <log2aayusharyan@gmail.com>
2020-11-03 09:46:35 +0530

    added. a program to get System Time

---
+++ BasicPythonsProgram/GetSystemTime.py
@@ -0,0 +1,14 @@
+"""
+author - ${Aayush Aryan}
+date - ${02-11-2020}
+time - ${08:15pm}
+package - ${Basicpython}
+Title -"Write a Python program to get the system time."
+"""
+import time
+try:
+ print()
+ print(time.ctime())
+ print()
+except:
+    print("checkedInbuiltImportStatementAndFunction")
