84ac5bcc7b9e7d021e2c5cd5251b634dd952e193
aayush-aryan <log2aayusharyan@gmail.com>
2020-11-03 10:46:25 +0530

    added. a program to get user environment

---
+++ BasicPythonsProgram/GetUserEnviroment.py
@@ -0,0 +1,15 @@
+"""
+author - ${Aayush Aryan}
+date - ${03-11-2020}
+time - ${10:15am}
+package - ${Basicpython}
+Title -"Write a Python program to get the users environment."
+"""
+import os
+try:
+ print()
+ print(os.environ)
+ print()
+except :
+    print("checkedImportStatementAndInbuiltFunction")
+
