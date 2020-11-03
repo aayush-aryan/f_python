9c68bb5386225c5fe49bdd6347d6d9ed0aab808b
aayush-aryan <log2aayusharyan@gmail.com>
2020-11-03 09:21:42 +0530

    added. a program to find current value of Recursion Limit

---
+++ BasicPythonsProgram/CurrentValueRecursionLimit.py
@@ -0,0 +1,15 @@
+"""
+author - ${Aayush Aryan}
+date - ${02-11-2020}
+time - ${07:55pm}
+package - ${Basicpython}
+Title -"Write a Python program to get the current value of the recursion limit."
+"""
+import sys
+try:
+ print()
+ print("Current value of the recursion limit:")
+ print(sys.getrecursionlimit())
+ print()
+except:
+    print("checkedImportStatementAndInbuiltMethod")
