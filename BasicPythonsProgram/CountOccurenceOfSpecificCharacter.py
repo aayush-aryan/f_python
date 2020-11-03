536a0cb0e2fd873c417525b0b0d82527f971f20e
aayush-aryan <log2aayusharyan@gmail.com>
2020-11-03 09:37:41 +0530

    added. a program to find specific occurence of letter in the string

---
+++ BasicPythonsProgram/CountOccurenceOfSpecificCharacter.py
@@ -0,0 +1,14 @@
+"""
+author - ${Aayush Aryan}
+date - ${02-11-2020}
+time - ${08:05pm}
+package - ${Basicpython}
+Title -"Write a Python program to count the number occurrence of a specific character in a string."
+"""
+GivenString = "Aayush Aryan knows Java as well as shellScript"
+try:
+ print()
+ print(GivenString.count("a"))
+ print()
+except:
+    print("ProvideProperStringAndCheckedInBuiltFunction")
