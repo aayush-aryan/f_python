d80fb28d83cbdf39d62118a72b88b73516f69fe2
aayush-aryan <log2aayusharyan@gmail.com>
2020-11-10 02:11:27 +0530

    added. a program to convert a list into the tuple

---
+++ Tuple/ConvertListToTuple.py
@@ -0,0 +1,27 @@
+"""
+author -aayush aryan
+date -09-11-2020
+time -11:00
+package -Tuple
+problem Statement-Write a Python program to convert a list to a tuple.
+"""
+
+class ListToTuple:
+
+    #Create method checkTuple
+    def toTuple(self,ListOfNum):
+        """
+                          :param ListOfNum: element the value
+                          :return: tuple value
+                         """
+        tupleOfNum = tuple(ListOfNum)
+        print("Tuple Data : ",tupleOfNum)
+listData = ListToTuple()
+rangeVal = int(input("Enter The range of value you want to enter : "))
+ListOfNum = [ ]
+for i in range(rangeVal):
+    val = int(input("Enter The value for tuple : "))
+    ListOfNum.append(val)
+
+print("List Data :- ",ListOfNum)
+listData.toTuple(ListOfNum)
