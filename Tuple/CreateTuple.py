0b499a5b096c599f40eed88d5c9d028e11f09438
aayush-aryan <log2aayusharyan@gmail.com>
2020-11-10 02:15:03 +0530

    added. a program to create a tuple

---
+++ Tuple/CreateTuple.py
@@ -0,0 +1,25 @@
+"""
+author -aayush aryan
+date -09-11-2020
+time -11:15
+package -Tuple
+problem Statement-'Write a Python program to create a tuple.'
+"""
+
+class CreateTuple:
+
+    #Create method
+    def create(self,rangeVal):
+        """
+                        :param rangeVal: element the value
+                        :return: tuple value
+                       """
+        tupleValue = ( )
+        for i in range(rangeVal):
+            val = int(input("Enter The Int value for tuple : "))
+            tupleValue = tupleValue[ : i] + (val,) + tupleValue[ i : ]
+        print("Tuple Data :- ", tupleValue)
+
+tupleData = CreateTuple()
+rangeVal = int(input("Enter The range of value you want to enter : "))
+tupleData.create(rangeVal)
