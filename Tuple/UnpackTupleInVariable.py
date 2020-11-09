3903ad37e02d8db78370168bba4bd13e3cb7a468
aayush-aryan <log2aayusharyan@gmail.com>
2020-11-10 02:30:47 +0530

    added. a program to unpack tuple in variable

---
+++ Tuple/UnpackTupleInVariable.py
@@ -0,0 +1,25 @@
+"""
+author -aayush aryan
+date -09-11-2020
+time -12:05
+package -Tuple
+problem Statement-Write a Python program to unpack a tuple in several variables.
+"""
+
+
+# function takes normal arguments
+# and multiply them
+def result(x, y):
+    return x * y
+
+
+# function with normal variables
+print(result(10, 100))
+
+# A tuple is created
+z = (10, 100)
+
+# Tuple is passed
+# function unpacked them
+
+print(result(*z))
