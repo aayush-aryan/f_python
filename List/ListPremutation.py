adc328fdf4c0cf225ede97b4358611aab5f604ea
aayush-aryan <log2aayusharyan@gmail.com>
2020-11-09 21:02:45 +0530

    added. a program to find premutation of the string

---
+++ List/ListPremutation.py
@@ -0,0 +1,30 @@
+"""
+author -aayush aryan
+date -07-11-2020
+time -15:10
+package -List
+problem Statement-'Write a Python program to generate all permutations of a list in Python.'
+"""
+
+import itertools
+class ListPermutation:
+
+    #this method calculate the permutation
+    def getPermutation(self, listOfStr):
+        """
+        :param listOfStr:
+        :return:
+        """
+        permList = (list(itertools.permutations(list1)))
+        return permList
+
+
+rangeVal = int(input("Enter the no of element in the list : "))
+list1 = []
+for i in range(rangeVal):
+    value = input("Enter The element: ")
+    list1.append(value)
+
+listPermutation = ListPermutation()
+print("List  : ", list1)
+print("The Updated List Are : ", listPermutation.getPermutation(list1))
