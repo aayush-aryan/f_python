f096823debbe19704500ddad2aaa57a492ed9a79
aayush-aryan <log2aayusharyan@gmail.com>
2020-11-10 02:10:00 +0530

    added. a program to find the colon of a tuple

---
+++ Tuple/ColonOfTuple.py
@@ -0,0 +1,31 @@
+"""
+author -aayush aryan
+date -09-11-2020
+time -10:200
+package -Tuple
+problem Statement-Write a Python program to create the colon of a tuple.
+"""
+
+from copy import deepcopy
+class CreateColon:
+
+    #method to find colon of a tuple
+    def createColon(self,intVal,floatVal,emptyList,strVal,boolVal):
+
+        tupleValue = (intVal, floatVal, emptyList, strVal, boolVal)
+        print("Tuple Data : ", tupleValue)
+        print()
+        tuplex_colon = deepcopy(tupleValue)
+
+        tuplex_colon[2].append(6)
+        print("Copied Tuple : ",tuplex_colon)
+
+create_colon = CreateColon()
+intVal = int(input("Enter The Int Value : "))
+floatVal = float(input("Enter The Float Value : "))
+
+emptyList = []
+strVal = input("Enter A String  : ")
+boolVal = bool(input("Enter The Boolean Value True or False : "))
+
+create_colon.createColon(intVal,floatVal,emptyList,strVal,boolVal)
