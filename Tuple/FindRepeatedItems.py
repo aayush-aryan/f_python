f19ccb58f3a5daf5df8f3c1bbc80e6f83bf43200
aayush-aryan <log2aayusharyan@gmail.com>
2020-11-10 02:18:08 +0530

    added. a program to find repeated items in tuple

---
+++ Tuple/FindRepeatedItems.py
@@ -0,0 +1,37 @@
+"""
+author -aayush aryan
+date -09-11-2020
+time -11:50
+package -Tuple
+problem Statement-'write a Python program to find the repeated items of a tuple'
+"""
+
+class RepeatedItem:
+
+    #Create Method to count the number
+    def findRepetedItem(self,tupleValue,findVal):
+        '''
+        :param:tupleValue: colection of Tuple Element
+        '''
+        countValue = tupleValue.count(findVal)
+        print("Count Of Value ",findVal,"Is : ",countValue)
+
+try:
+
+    tupleData = RepeatedItem()
+    rangeVal = int(input("Enter The range of value you want to enter : "))
+    tupleValue = ( )
+    for j in range(rangeVal):
+
+       val = int(input("Enter The Int value for tuple : "))
+       tupleValue = tupleValue[ : j] + (val,) + tupleValue[ j : ]
+
+    print("Tuple Data :- ", tupleValue)
+
+    findVal = int(input("Enter value you want to Count : "))
+
+
+
+    tupleData.findRepetedItem(tupleValue,findVal)
+except:
+    print("MisMatchInput")
