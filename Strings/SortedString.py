8252d9ce1210233ae28b8a675c860623ddf763f3
aayush-aryan <log2aayusharyan@gmail.com>
2020-11-10 10:19:39 +0530

    added. a program to sort the String

---
+++ Strings/SortedString.py
@@ -0,0 +1,24 @@
+"""
+author -aayush aryan
+date -09-11-2020
+time -15:10
+package -String
+problem Statement-Write a Python program that accepts a comma separated sequence of words as input
+and prints the unique words in sorted form (alphanumerically).
+Sample Words : red, white, black, red, green, black
+Expected Result : black, green, red, white,red.
+"""
+
+class SortedWords:
+    def findUnique(self,item):
+        words = [word for word in items.split(",")]
+        print(",".join(sorted(list(set(words)))))
+
+
+if __name__ == '__main__':
+    unique_word=SortedWords()
+    try:
+        items = input("Input comma separated sequence of words: ")
+        unique_word.findUnique(items)
+    except:
+        print("SetValueError")
