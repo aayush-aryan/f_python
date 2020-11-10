86c2d21f0ee35ca9b2cb8c28fdfcf081d7d962dc
aayush-aryan <log2aayusharyan@gmail.com>
2020-11-10 10:14:10 +0530

    added. a program to convert formated text

---
+++ Strings/FormatedText.py
@@ -0,0 +1,15 @@
+"""
+author -aayush aryan
+date -09-11-2020
+time -12:10
+package -Strings
+problem Statement-'Write a Python program to display formatted text (width=50) as output..
+"""
+
+import textwrap
+sample_text = '''
+  GitHub, Inc. is an American multinational corporation that provides hosting for software development and version control using Git. It offers the distributed version control and source code management (SCM) functionality of Git, plus its own features. It provides access control and several collaboration features such as bug tracking, feature requests, task management, continuous integration and wikis for every project.[4] Headquartered in California, it has been a subsidiary of Microsoft since 2018.[5
+  '''
+print()
+print(textwrap.fill(sample_text, width=50))
+print()
